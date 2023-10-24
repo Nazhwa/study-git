380919a01f18e9c0f858cc10bd40ed711209d39d
Nazhwa <nazhwanursyabrina@gmail.com>
2023-10-24 13:27:51 +0700

    feat:add nazhwa.js

--- repository/nazhwa.js
+++ repository/nazhwa.js
@@ -1,2 +1,3 @@
 console.log("Selamat Datang");
 console.log("Nazhwa Nur Syabrina");
+console.log("Aku Berusisa 18 tahun");
--- repository/tulus.js
+++ repository/tulus.js
@@ -2,4 +2,5 @@ const tulus = {
     name : "tulus",
     pekerjaan : "penyanyi",
     usia : 36,
+    lahir : "dua puluh agustus"
 }
